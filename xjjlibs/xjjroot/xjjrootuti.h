efbe035f5d7d40c9fb766e261f19386f0168d523
Jing <boundino@gmail.com>
2017-07-26 22:00:46 -0400

    organize folder

---
+++ xjjlibs/xjjroot/xjjrootuti.h
@@ -0,0 +1,182 @@
+#ifndef _XJJROOT_UTI_H_
+#define _XJJROOT_UTI_H_
+
+/* xjjrootuti.h */
+
+#include <TTree.h>
+#include <TLatex.h>
+#include <TLegend.h>
+#include <TLine.h>
+#include <TStyle.h>
+
+namespace xjjroot
+{
+  const float x_tex_left_top = 0.23;
+  const float y_tex_left_top = 0.91;
+  const float dy_tex_left_top = 0.06;
+
+  void setgstyle();
+  template <class T>
+  void sethempty(T* hempty, Float_t xoffset=0, Float_t yoffset=0);
+  template <class T>
+  void setthgr(T* hempty, Float_t xoffset=0, Float_t yoffset=0);
+  template <class T>
+  void setthgrstyle(T* h, Color_t mlcolor, Style_t mstyle=-1, Size_t msize=-1, Style_t lstyle=-1, Width_t lwidth=-1);
+  void drawCMS(TString collision, TString snn="5.02");
+  void settex(TLatex* tex, Float_t tsize=0.04, Short_t align=12);
+  void settexndraw(TLatex* tex, Float_t tsize=0.04, Short_t align=12);
+  void drawtex(Double_t x, Double_t y, const char *text, Float_t tsize=0.04, Short_t align=12);
+  void setleg(TLegend* leg, Float_t tsize=0.04);
+  void setlegndraw(TLegend* leg, Float_t tsize=0.04);
+  void setline(TLine* l, Color_t lcolor=kBlack, Style_t lstyle=1, Width_t lwidth=2);
+  void setlinendraw(TLine* l, Color_t lcolor=kBlack, Style_t lstyle=1, Width_t lwidth=2);
+  void drawline(Double_t x1, Double_t y1, Double_t x2, Double_t y2, Color_t lcolor=kBlack, Style_t lstyle=1, Width_t lwidth=2);
+
+  void setbranchaddress(TTree* nt, const char* bname, void* addr);
+}
+
+/* ---------- */
+
+void xjjroot::setgstyle()
+{
+  gStyle->SetOptTitle(0);
+  gStyle->SetOptStat(0);
+  gStyle->SetEndErrorSize(0);
+  gStyle->SetTextSize(0.05);
+  gStyle->SetTextFont(42);
+  gStyle->SetPadRightMargin(0.043);
+  gStyle->SetPadLeftMargin(0.18);
+  gStyle->SetPadTopMargin(0.1);
+  gStyle->SetPadBottomMargin(0.145);
+  gStyle->SetTitleX(.0f);
+}
+
+template <class T>
+void xjjroot::sethempty(T* hempty, Float_t xoffset/*=0*/, Float_t yoffset/*=0*/)
+{
+  hempty->GetXaxis()->CenterTitle();
+  hempty->GetYaxis()->CenterTitle();
+  hempty->GetXaxis()->SetTitleOffset(1.10+xoffset);
+  hempty->GetYaxis()->SetTitleOffset(1.30+yoffset);
+  hempty->GetXaxis()->SetTitleSize(0.05);
+  hempty->GetYaxis()->SetTitleSize(0.05);
+  hempty->GetXaxis()->SetTitleFont(42);
+  hempty->GetYaxis()->SetTitleFont(42);
+  hempty->GetXaxis()->SetLabelFont(42);
+  hempty->GetYaxis()->SetLabelFont(42);
+  hempty->GetXaxis()->SetLabelSize(0.045);
+  hempty->GetYaxis()->SetLabelSize(0.045);
+  hempty->SetStats(0);
+}
+
+template <class T>
+void xjjroot::setthgr(T* hempty, Float_t xoffset/*=0*/, Float_t yoffset/*=0*/)
+{
+  hempty->GetXaxis()->CenterTitle();
+  hempty->GetYaxis()->CenterTitle();
+  hempty->GetXaxis()->SetTitleOffset(1.10+xoffset);
+  hempty->GetYaxis()->SetTitleOffset(1.30+yoffset);
+  hempty->GetXaxis()->SetTitleSize(0.05);
+  hempty->GetYaxis()->SetTitleSize(0.05);
+  hempty->GetXaxis()->SetTitleFont(42);
+  hempty->GetYaxis()->SetTitleFont(42);
+  hempty->GetXaxis()->SetLabelFont(42);
+  hempty->GetYaxis()->SetLabelFont(42);
+  hempty->GetXaxis()->SetLabelSize(0.045);
+  hempty->GetYaxis()->SetLabelSize(0.045);
+  hempty->SetStats(0);
+  hempty->SetMarkerStyle(20);
+  hempty->SetMarkerSize(1.1);
+  hempty->SetMarkerColor(kBlack);
+  hempty->SetLineColor(kBlack);
+}
+
+template <class T>
+void xjjroot::setthgrstyle(T* h, Color_t mlcolor, Style_t mstyle/*=-1*/, Size_t msize/*=-1*/, Style_t lstyle/*=-1*/, Width_t lwidth/*=-1*/)
+{
+  h->SetMarkerColor(mlcolor);
+  h->SetLineColor(mlcolor);
+  if(mstyle>=0) h->SetMarkerStyle(mstyle);
+  if(lstyle>=0) h->SetLineStyle(lstyle);
+  if(msize>=0)  h->SetMarkerSize(msize);
+  if(lwidth>=0) h->SetLineWidth(lwidth);
+}
+
+void xjjroot::drawCMS(TString collision, TString snn/*="5.02"*/)
+{
+  TLatex* texCms = new TLatex(0.18,0.93, "#scale[1.25]{CMS} Preliminary");
+  texCms->SetNDC();
+  texCms->SetTextAlign(12);
+  texCms->SetTextSize(0.04);
+  texCms->SetTextFont(42);
+  texCms->Draw();
+  TLatex* texCol = new TLatex(0.96,0.93, Form("%s #sqrt{s_{NN}} = %s TeV", collision.Data(), snn.Data()));
+  texCol->SetNDC();
+  texCol->SetTextAlign(32);
+  texCol->SetTextSize(0.04);
+  texCol->SetTextFont(42);
+  texCol->Draw();
+}
+
+void xjjroot::settex(TLatex* tex, Float_t tsize/*=0.04*/, Short_t align/*=12*/)
+{
+  tex->SetNDC();
+  tex->SetTextFont(42);
+  tex->SetTextAlign(align);
+  tex->SetTextSize(tsize);
+}
+
+void xjjroot::settexndraw(TLatex* tex, Float_t tsize/*=0.04*/, Short_t align/*=12*/)
+{
+  xjjroot::settex(tex, tsize, align);
+  tex->Draw();
+}
+
+void xjjroot::drawtex(Double_t x, Double_t y, const char* text, Float_t tsize/*=0.04*/, Short_t align/*=12*/)
+{
+  TLatex* tex = new TLatex(x, y, text);
+  xjjroot::settexndraw(tex, tsize, align);
+}
+
+void xjjroot::setleg(TLegend* leg, Float_t tsize/*=0.04*/)
+{
+  leg->SetBorderSize(0);
+  leg->SetFillStyle(0);
+  leg->SetTextFont(42);
+  leg->SetTextSize(tsize);
+}
+
+void xjjroot::setlegndraw(TLegend* leg, Float_t tsize/*=0.04*/)
+{
+  xjjroot::setleg(leg, tsize);
+  leg->Draw();
+}
+
+void xjjroot::setline(TLine* l, Color_t lcolor/*=kBlack*/, Style_t lstyle/*=1*/, Width_t lwidth/*=2*/)
+{
+  l->SetLineColor(lcolor);
+  l->SetLineStyle(lstyle);
+  l->SetLineWidth(lwidth);
+}
+
+void xjjroot::setlinendraw(TLine* l, Color_t lcolor/*=kBlack*/, Style_t lstyle/*=1*/, Width_t lwidth/*=2*/)
+{
+  xjjroot::setline(l, lcolor, lstyle, lwidth);
+  l->Draw();
+}
+
+void xjjroot::drawline(Double_t x1, Double_t y1, Double_t x2, Double_t y2, Color_t lcolor/*=kBlack*/, Style_t lstyle/*=1*/, Width_t lwidth/*=2*/)
+{
+  TLine* l = new TLine(x1, y1, x2, y2);
+  xjjroot::setlinendraw(l, lcolor, lstyle, lwidth);
+}
+
+/* ----- */
+
+void xjjroot::setbranchaddress(TTree* nt, const char* bname, void* addr)
+{
+  nt->SetBranchStatus(bname, 1);
+  nt->SetBranchAddress(bname, addr);
+}
+
+#endif
