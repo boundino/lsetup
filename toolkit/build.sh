e279e779448bc36526de810560b610996cc394ee
Jing <boundino@gmail.com>
2019-08-15 15:19:04 -0400

    update color/psme/build

--- toolkit/build.sh
+++ toolkit/build.sh
@@ -4,20 +4,25 @@
 
 # CMSSW_10_3_3_patch1 CMSSW_9_4_13_patch1 CMSSW_8_0_24
 
-[[ $# -ne 1 ]] && return
-cmsver=$1
+folder=root
+cmsver=
+if [[ $# -eq 1 ]]
+then
+    cmsver=$1
+    echo
+    echo -e "Processing \e[36;4;1m$cmsver\e[0m >>>"
+    cmsrel ${cmsver} || return
+    cd ${cmsver}/src/
+    cmsenv
+    cd ../../
+    folder=$cmsver
+fi
 
-echo
-echo -e "Processing \e[36;4;1m$cmsver\e[0m >>>"
-cmsrel ${cmsver} || return
-cd ${cmsver}/src/
-cmsenv
-cd ../../
-
-mkdir -p exec/${cmsver}
+mkdir -p exec/$folder
 for i in `echo *.C` ; do 
     echo "building $i ..."
-    g++ $i $(root-config --libs --cflags) -g -o exec/${cmsver}/${i%%.C}
+    g++ $i $(root-config --libs --cflags) -g -o exec/${folder}/${i%%.C}
 done
 
-rm -rf $cmsver
+[[ x$cmsver == x ]] || rm -rf $cmsver
+
