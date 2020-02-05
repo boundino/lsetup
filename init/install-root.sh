f54fdf85d57ca5999c9a79a31c90392ddbc06081
Jing <boundino@gmail.com>
2020-02-05 14:57:20 -0500

    add install cmake in root install

--- init/install-root.sh
+++ init/install-root.sh
@@ -3,10 +3,18 @@
 # https://root.cern.ch/get-root-sources
 # https://root.cern.ch/building-root
 
+# Install cmake if not yet done
+brew install cmake
+
+# Get root source macros
 git clone http://github.com/root-project/root.git
+
+# Build root
 mkdir buildroot
 cd buildroot
 cmake ../root
 cmake --build .
+
+# Initialization
 readlink dir/bin/thisroot.sh
-# source /path/to/build/dir/bin/thisroot.sh
+source $(readlink dir/bin/thisroot.sh)
