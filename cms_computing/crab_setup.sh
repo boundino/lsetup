caaa6bcf2a4b6c96c674a388557954b93b3ed921
Jing <boundino@gmail.com>
2020-03-09 23:48:27 +0100

    update crab_setup

--- cms_computing/crab_setup.sh
+++ cms_computing/crab_setup.sh
@@ -1,3 +1,3 @@
-[[ "$(ls -lt /tmp/ | grep --color=no $USER | grep --color=no -m 1 x509)x" == x ]] && { voms-proxy-init --voms cms --valid 168:00 ; voms-proxy-info --all ; }
+[[ $(ls -lt /tmp/ | grep --color=no "$USER " | grep --color=no -m 1 x509)x == x ]] && { voms-proxy-init --voms cms --valid 168:00 ; voms-proxy-info --all ; }
 source /cvmfs/cms.cern.ch/crab3/crab.sh
 which crab
