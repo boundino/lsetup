7679141feaed5b62175533535e291729199a336c
Jing Wang <boundino@gmail.com>
2020-09-19 05:02:49 +0200

    don't use rm -v as rm

--- cms_computing/crab_setup.sh
+++ cms_computing/crab_setup.sh
@@ -1,3 +1,4 @@
-[[ $(ls -lt /tmp/ | grep --color=no "$USER " | grep --color=no -m 1 x509)x == x ]] && { voms-proxy-init --voms cms --valid 168:00 ; voms-proxy-info --all ; }
+[[ $(ls -lt /tmp/ | grep --color=no "$USER " | grep --color=no -m 1 x509)x == x ]] && { voms-proxy-init --voms cms --valid 168:00 ; }
+voms-proxy-info --all
 source /cvmfs/cms.cern.ch/crab3/crab.sh
-which crab
+echo -e "\e[32;1m$(which crab)\e[0m"
