bd1aecc837a0494fd8dc775e0f04483af111eab5
Jing <boundino@gmail.com>
2020-09-27 14:06:22 -0400

    add more pip3 install modules

--- init/install-python3.sh
+++ init/install-python3.sh
@@ -11,9 +11,19 @@ pip3 install --upgrade pipenv
 pip3 install requests_html
 pip3 install requests
 
+# install math modules
+pip3 install numpy
+pip3 install scipy
+pip3 install matplotlib
+pip3 install h5py
+pip3 install lmfit
+
 # open another shell
 # pipenv shell
 # pipenv install requests_html
 # pipenv install requests
 
+# jupyter notebook
 pip3 install jupyterlab
+pip3 install notebook
+
