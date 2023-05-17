82d23dc9ef6f4dabd8d06a37a77b2f4314aecf0e
boundino <boundino@gmail.com>
2023-05-17 14:47:24 +0200

    fix build .cc .C

--- toolkit/build.sh
+++ toolkit/build.sh
@@ -19,7 +19,7 @@ then
 fi
 
 mkdir -p exec/$folder
-for i in `echo *.cc` ; do 
+for i in `echo *.cc *.C` ; do 
     echo "building $i ..."
     g++ $i $(root-config --libs --cflags) -g -o exec/${folder}/${i%%.cc}
 done
