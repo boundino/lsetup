1559c0b89212f856441b23b95d2e6699070ead99
boundino <boundino@gmail.com>
2023-06-17 15:48:32 +0200

    up svn

--- cms_computing/svn/init.sh
+++ cms_computing/svn/init.sh
@@ -1,21 +1,22 @@
+#!/bin/bash
 
-eval `./notes/tdr runtime -sh`
+[[ $0 != *bash* ]] && { echo use . init.sh or source init.sh; return 1; }
+
+AN="AN-15-308"
+CADI="HIN-16-001"
+
+# >>>>>>>>>>>>>>> #
+SUBFOLDER=""
+DOCNO=""
 
 TYPE=${1:-"note"}
-if [[ $TYPE = "note" ]]
-then
-    SUBFOLDER="notes"
-    CADI="AN-15-308"
-elif [[ $TYPE = "paper" ]]
-then
-    SUBFOLDER="papers"
-    CADI="HIN-16-001"
-elif [[ $TYPE = "pas" ]]
-then
-    SUBFOLDER="notes"
-    CADI="HIN-16-001"
-else
-    return 1;
-fi
+[[ $TYPE == "note" ]] && { SUBFOLDER="notes"; DOCNO=$AN; }
+[[ $TYPE == "paper" ]] && { SUBFOLDER="papers"; DOCNO=$CADI; }
+[[ $TYPE == "pas" ]] && { SUBFOLDER="notes"; DOCNO=$CADI; }
 
-cd ${SUBFOLDER}/${CADI}/trunk/
+[[ "x$DOCNO" == "x" || "x$SUBFOLDER" == "x" ]] && { echo invalid TYPE of document [note/paper/pas]; return 1; }
+
+[[ ! -d ./${SUBFOLDER} ]] && { echo no ./${SUBFOLDER} is found.; return 1; }
+
+eval `./${SUBFOLDER}/tdr runtime -sh`
+cd ${SUBFOLDER}/${DOCNO}/trunk/
