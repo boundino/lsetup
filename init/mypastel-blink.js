ad6fd98f599247cf68d054b2dd17d1d65b413ff6
boundino <boundino@gmail.com>
2025-01-15 19:29:16 +0100

    add blink theme js

---
+++ init/mypastel-blink.js
@@ -0,0 +1,26 @@
+black       = '#616161';
+red         = '#ee8978';
+green       = '#d0f99f';
+yellow      = '#fefdc8';
+blue        = '#afd4fa';
+magenta     = '#f095f7';
+cyan        = '#a3fcf4';
+white       = '#f1f1f1';
+lightBlack  = '#8e8e8e';
+lightRed    = '#f5c6bf';
+lightGreen  = '#ddfbc0';
+lightYellow = '#fefdd9';
+lightBlue   = '#c8e2fc';
+lightMagenta= '#f3b5f9';
+lightCyan   = '#b8fcf0';
+lightWhite  = '#fefefe';
+
+t.prefs_.set('color-palette-overrides',
+             [ black, red, green, yellow,
+               blue, magenta, cyan, white, 
+               lightBlack, lightRed, lightGreen, lightYellow,
+               lightBlue, lightMagenta, lightCyan, lightWhite ]); 
+
+t.prefs_.set('foreground-color', "#e5e5e5");
+t.prefs_.set('background-color', "#000000");
+t.prefs_.set('cursor-color', 'rgba(243,183,125,0.5)');
